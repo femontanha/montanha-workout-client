c2bd7d5190bdfaf0df47e3be54d727eaecb6b38a
Fellipe Azambuja <femontanhanew@gmail.com>
2022-11-26 18:42:08 -0300

    Add style in Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import Head from 'next/head'
+import { Container } from '../components/Container'
 import styles from './Home.module.css'
+import { FiAlertTriangle } from 'react-icons/fi'
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>femontanha workout</title>
       </Head>
       <main className={styles.main}>
-        Home
+        <div className={styles.painel}>
+          <FiAlertTriangle className={styles.icon} />
+          <span className={styles.text}>Nothing to do here ...</span>
+        </div>
       </main>
-    </div>
+    </Container>
   )
 }
 
